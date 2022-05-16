7261f0fc84c47f62f16b111163ae00a160205f98
arseyrc <arseyrc@gmail.com>
2022-05-16 16:00:55 -0400

    Week 3 Demo A Added

---
+++ 02Week03DemoA - MyAdder/frmMain.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace _02Week03DemoA___MyAdder
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeader = new System.Windows.Forms.Label();
+            this.lblX = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.lblPlus = new System.Windows.Forms.Label();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.lblY = new System.Windows.Forms.Label();
+            this.lblEquals = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblHeader
+            // 
+            this.lblHeader.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblHeader.BackColor = System.Drawing.Color.SteelBlue;
+            this.lblHeader.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblHeader.Location = new System.Drawing.Point(0, 0);
+            this.lblHeader.Name = "lblHeader";
+            this.lblHeader.Size = new System.Drawing.Size(435, 28);
+            this.lblHeader.TabIndex = 0;
+            this.lblHeader.Text = "My Adder";
+            this.lblHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(121, 52);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(14, 15);
+            this.lblX.TabIndex = 1;
+            this.lblX.Text = "&X";
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(121, 71);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(31, 23);
+            this.txtX.TabIndex = 2;
+            // 
+            // lblPlus
+            // 
+            this.lblPlus.AutoSize = true;
+            this.lblPlus.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblPlus.Location = new System.Drawing.Point(158, 67);
+            this.lblPlus.Name = "lblPlus";
+            this.lblPlus.Size = new System.Drawing.Size(25, 25);
+            this.lblPlus.TabIndex = 3;
+            this.lblPlus.Text = "+";
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(189, 71);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(31, 23);
+            this.txtY.TabIndex = 5;
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Location = new System.Drawing.Point(189, 52);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(14, 15);
+            this.lblY.TabIndex = 4;
+            this.lblY.Text = "&Y";
+            // 
+            // lblEquals
+            // 
+            this.lblEquals.AutoSize = true;
+            this.lblEquals.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblEquals.Location = new System.Drawing.Point(228, 67);
+            this.lblEquals.Name = "lblEquals";
+            this.lblEquals.Size = new System.Drawing.Size(25, 25);
+            this.lblEquals.TabIndex = 6;
+            this.lblEquals.Text = "=";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAnswer.Location = new System.Drawing.Point(259, 71);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(60, 23);
+            this.lblAnswer.TabIndex = 7;
+            this.lblAnswer.Text = "-";
+            this.lblAnswer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnCalculate.Location = new System.Drawing.Point(103, 125);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(72, 46);
+            this.btnCalculate.TabIndex = 8;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnReset.Location = new System.Drawing.Point(181, 125);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(72, 46);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Re&set";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.SteelBlue;
+            this.btnExit.Location = new System.Drawing.Point(259, 125);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(72, 46);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmMain
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnReset;
+            this.ClientSize = new System.Drawing.Size(434, 211);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblEquals);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblPlus);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.lblHeader);
+            this.MaximumSize = new System.Drawing.Size(450, 250);
+            this.MinimumSize = new System.Drawing.Size(450, 250);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Week 3 - My Addition Tool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblHeader;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.Label lblPlus;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Label lblEquals;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ 02Week03DemoA - MyAdder/frmMain.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02Week03DemoA___MyAdder
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// EVENT: Closes the application
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        /// <summary>
+        /// EVENT: Resets the entire form back to its default values
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            //txtX.Text = "";
+            //txtX.Text = string.Empty;
+            txtX.Clear();  // most efficient 
+            txtY.Clear();
+            lblAnswer.Text = "-";
+            txtX.Focus();
+        }
+
+        /// <summary>
+        /// EVENT:   Performs the actual addition of the values in X and Y textboxes and
+        ///          places the answer in the answer label
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int numX, numY, answer;
+
+            if (int.TryParse(txtX.Text, out numX))
+            {
+                // it worked
+                if (int.TryParse(txtY.Text, out numY))
+                {
+                    // it also worked
+                    answer = numX + numY;
+                    lblAnswer.Text = answer.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("The value of Y is not an integer!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("The value of X is not an integer!");
+            }
+        }
+    }
+}
