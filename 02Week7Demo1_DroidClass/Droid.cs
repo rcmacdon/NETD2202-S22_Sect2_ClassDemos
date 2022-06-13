95527895f5db99b6fd854dbb8718dca6afb87397
arseyrc <arseyrc@gmail.com>
2022-06-13 16:02:27 -0400

    Week 7 - Demo 1 - Droid Class added

---
+++ 02Week7Demo1_DroidClass/Droid.cs
@@ -0,0 +1,97 @@
+﻿/* Name: Clint MacDonald
+ * Date: June 13, 2022
+ * Title: Droid Class
+ * Purpose: NETD2202 Week 7 Class Demo
+ */
+
+#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+#endregion
+
+namespace _02Week7Demo1_DroidClass
+{
+    class Droid
+    {
+        #region Global Variables
+        //protected vs private vs public
+        public const int MinDesignationLength = 3;
+        public const int MaxDesignationLength = 8;
+        public const int MinOwnerLength = 4;
+
+        #endregion
+
+        #region Properties
+        // long way - commented out as it will be a duplicate
+        //private string designation;
+        //public string Designation
+        //{
+        //    get { return designation; }
+        //    set { designation = value; }
+        //}
+
+        public string Designation { get; set; }
+        public string Owner { get; set; }
+        public bool IsInService { get; set; }
+        public Color PrimaryColor { get; set; }
+        public Color SecondaryColor { get; set; }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public Droid() 
+        {
+            SetDefaults();
+        }
+        /// <summary>
+        /// Parameterized Constructor
+        /// </summary>
+        /// <param name="designation"></param>
+        /// <param name="owner"></param>
+        /// <param name="isInService"></param>
+        /// <param name="primaryColor"></param>
+        /// <param name="secondaryColor"></param>
+        public Droid(string designation, string owner, bool isInService, Color primaryColor, Color secondaryColor)
+        {
+            Designation = designation;
+            Owner = owner;
+            IsInService = isInService;
+            PrimaryColor = primaryColor;
+            SecondaryColor = secondaryColor;
+        }
+        #endregion
+
+        #region Custom Methods
+        private void SetDefaults()
+        {
+            Designation = string.Empty;
+            Owner = string.Empty;
+            IsInService = false;
+            PrimaryColor = Color.Gray;
+            SecondaryColor = Color.Gray;
+        }
+
+        public void SellDroid(string owner, bool isInService)
+        {
+            Owner = owner;
+            IsInService = isInService;
+        }
+        #endregion
+
+        #region Static Methods
+        public static Droid FindDroid(string desgination)
+        {
+            Droid returnDroid = new Droid();
+
+            // search for droid
+            return returnDroid;
+        }
+        #endregion
+    }
+}
---
+++ 02Week7Demo1_DroidClass/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02Week7Demo1_DroidClass
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void something()
+        {
+            Droid drd = new Droid();
+            drd.Designation = "R2D2";
+            drd.Owner = "Obiwon Kenobi";
+            drd.IsInService = true;
+            drd.PrimaryColor = Color.White;
+            drd.SecondaryColor = Color.Blue;
+
+            Droid drd2 = new Droid("C3P0", "Anakin Skywalker", true, Color.Gold, Color.Black);
+
+            // Anakin goes all dark on us and C3P0 goes to Luke
+            drd2.SellDroid("Luke Skywalker", true);
+
+            Droid foundDroid = Droid.FindDroid("R2D2");
+
+        }
+
+    }
+}
