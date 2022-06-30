a35733786fc0036b47ba5316469ba424e61c508c
Clint <clint.macdonald@durhamcollege.ca>
2022-06-30 13:06:57 -0400

    Week 8 Demo 1 - Trooper Class

---
+++ 02Week08_Demo1_ClassesContinued/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02Week08_Demo1_ClassesContinued
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
